20c5df8ca6851706ae1b0cd10b99cbdf209e1db5
Joseph <yusefhalabi456@gmail.com>
2021-10-04 00:11:18 +0300

    Added buttons, display results

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+const btn = document.querySelector('button');
+const btns = document.querySelectorAll('button');
+
 function computerPlay(){
     var choicesArray = ["Rock", "Paper", "Scissors"];
     return choicesArray[Math.floor(Math.random() * choicesArray.length)];
@@ -41,30 +44,65 @@ function playRound(playerSelection, computerSelection){
     } 
 }
 
-function game(){
-    var playerScore=0;
-    var computerScore=0;
+// function game(){
+//     var playerScore=0;
+//     var computerScore=0;
+//     const res = document.querySelector('div');
+//     for(var i=0; i<5; i++){
+//         console.log("A new round is starting!");
+//         var outcome = UIgame();
 
-    for(var i=0; i<5; i++){
-        console.log("A new round is starting!");
-        var outcome = playRound(prompt("Enter your choice!"), computerPlay());
-        console.log(outcome);
-        if(outcome.includes("Win!", 4)){
-            playerScore++;
-        }
-        else{
-            computerScore++;
-        }
-    }
-    if(playerScore > computerScore){
-        console.log("You Won the game!");
-    }
-    else if(computerScore > playerScore){
-        console.log("You Lost the game!");
-    }
-    else{
-        console.log("It's a tie!");
-    }
+//         console.log(outcome);
+//         if(outcome.includes("Win!", 4)){
+//             playerScore++;
+//         }
+//         else{
+//             computerScore++;
+//         }
+//     }
+//     if(playerScore > computerScore){
+//         console.log("You Won the game!");
+//     }
+//     else if(computerScore > playerScore){
+//         console.log("You Lost the game!");
+//     }
+//     else{
+//         console.log("It's a tie!");
+//     }
+// }
+
+UIgame();
+const board = document.querySelector('.result');
+const result = document.createElement('h2');
+function displayResults(outcome){
+
+    result.textContent = outcome;
+    result.style.color = 'red';
+    board.appendChild(result);
+    setTimeout(function () {
+
+        board.removeChild(result);
+    }, 3000);
+    
+    
+    
+    
 }
 
-game();
+function UIgame(){
+    var playerScore=0;
+    var computerScore=0;
+    const res = document.querySelector('.result');
+
+    btns.forEach(btn => {
+        btn.addEventListener('click', (e)=> {
+            var outcome = playRound(e.target.innerText, computerPlay());
+            
+            displayResults(outcome);
+            
+           
+        })
+    });
+
+
+}
