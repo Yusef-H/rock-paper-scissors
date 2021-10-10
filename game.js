cc7b7c7cbce3d74df9d027f13d1f3c756a135d30
Joseph <yusefhalabi456@gmail.com>
2021-10-10 20:03:23 +0300

    Added scoreboard, working on win/lose after 5 rounds

--- game.js
+++ game.js
@@ -46,39 +46,67 @@ function playRound(playerSelection, computerSelection){
 
 
 
-UIgame();
+
 const board = document.querySelector('.result');
 const result = document.createElement('h2');
+const score = document.querySelector('.score h1');
+const player = document.createElement('div');
+const computer = document.createElement('div');
+UIgame();
+
 function displayResults(outcome){
 
     result.textContent = outcome;
-    result.style.color = 'red';
+    result.style.color = 'black';
     result.id = 'threeSeconds';
     board.appendChild(result);
     setTimeout(function(){
         document.getElementById('threeSeconds').className += ' hidden';
-    }, 5000);
+    }, 3000);
     document.getElementById('threeSeconds').className -= ' hidden';
-    
-    
-    
 }
 
+function updateScoreBoard(playerScore, computerScore){
+    player.innerHTML = "Player Score: " + playerScore;
+    computer.innerHTML = "Computer Score: "+ computerScore;
+    
+    score.appendChild(player);
+    score.appendChild(computer);
+}
+
+const playerWin = document.createElement('div');
+const computerWin = document.createElement('div');
+
 function UIgame(){
+    
     var playerScore=0;
     var computerScore=0;
+    updateScoreBoard(playerScore, computerScore);
     const res = document.querySelector('.result');
-    for(var i=0; i<5; i++){
+    
     btns.forEach(btn => {
         btn.addEventListener('click', (e)=> {
             var outcome = playRound(e.target.innerText, computerPlay());
             
             displayResults(outcome);
-            
-           
+            if(outcome.includes("Win!", 4)){
+                playerScore++;
+            }
+            else if(outcome.includes("Lose!", 4)){
+                computerScore++;
+            }
+            updateScoreBoard(playerScore, computerScore);
+
+            if(playerScore == 5){
+                playerWin.id = 'win';
+                document.getElementById('win').className += ' win';
+                
+
+
+            }
         })
     });
-}
+
 
 
 }
